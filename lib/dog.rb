3c577510779d7398a663593b680574ef942a5e29
Janay Bailey <tjanay87@gmail.com>
2019-10-04 13:27:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,6 +6,7 @@ class Dog
 
   @@all = []
   
+<<<<<<< HEAD
   def initialize(name)
     @name = name
     save
@@ -23,6 +24,22 @@ def save
      # puts dog.name
     #end
   #end
+=======
+   def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+>>>>>>> c276e085dd16eb8ee3bb57141b2e91227bb1ca78
 
   def self.clear_all
     @@all.clear
@@ -33,9 +50,18 @@ def save
     puts dog.name
   end
 
+<<<<<<< HEAD
   
+=======
+  def save
+     self.class.all << self
+  end
+>>>>>>> c276e085dd16eb8ee3bb57141b2e91227bb1ca78
 end
 end
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> c276e085dd16eb8ee3bb57141b2e91227bb1ca78
